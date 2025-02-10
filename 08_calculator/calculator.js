49655e1b3344bc5331880c994087de109ffebaac
Jack Neilson <jackdneilson@gmail.com>
2025-02-10 10:32:24 +0000

    feat: Add solution to exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,37 @@
-const add = function() {
-	
+const add = function (left, right) {
+	return left + right;
 };
 
-const subtract = function() {
-	
+const subtract = function (left, right) {
+	return left - right;
 };
 
-const sum = function() {
-	
+const sum = function (toSum) {
+	return toSum.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (toMultiply) {
+	return toMultiply.reduce((sum, current) => sum * current, 1);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+	let ret = 1;
+	for (let i = 2; i <= number; i++) {
+		ret = ret * i;
+	}
+	return ret;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
