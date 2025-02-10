93ceda407a485874f4d36adf2e6e345b69aeccbb
Jack Neilson <jackdneilson@gmail.com>
2025-02-10 15:18:36 +0000

    feat: Add solutions to exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+	let stripped = string.replace(/[!,\.\s*]/g, "").toLowerCase();
+	let reversed = stripped.split("").reverse().join("");
+	return stripped === reversed;
 };
 
 // Do not edit below this line
