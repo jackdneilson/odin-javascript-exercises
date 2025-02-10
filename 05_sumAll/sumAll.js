ceaffd74977188a25d017ec26bb67c04d5c65ea2
Jack Neilson <jackdneilson@gmail.com>
2025-02-10 10:04:25 +0000

    feat: Add solution to exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (lower, upper) {
+	if (typeof lower != "number" || typeof upper != "number") {
+		return "ERROR";
+	}
 
+	if (!Number.isInteger(lower) || !Number.isInteger(upper)) {
+		return "ERROR";
+	}
+
+	if (lower < 0 || upper < 0) {
+		return "ERROR";
+	}
+
+	if (lower > upper) {
+		[lower, upper] = [upper, lower];
+	}
+
+	let sum = 0;
+	for (let i = lower; i <= upper; i++) {
+		sum += i;
+	}
+
+	return sum;
 };
 
 // Do not edit below this line
