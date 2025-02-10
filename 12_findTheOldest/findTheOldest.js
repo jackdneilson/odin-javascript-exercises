20352d54d2f9e18f3397519ae239381849f6bed3
Jack Neilson <jackdneilson@gmail.com>
2025-02-10 19:46:24 +0000

    feat: Add solution for exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+	return people.reduce((prev, current) =>
+		getAge(current) > getAge(prev) ? current : prev,
+	);
+};
 
+const getAge = function (person) {
+	let yearOfDeath = person.yearOfDeath ?? new Date().getFullYear();
+	return yearOfDeath - person.yearOfBirth;
 };
 
 // Do not edit below this line
