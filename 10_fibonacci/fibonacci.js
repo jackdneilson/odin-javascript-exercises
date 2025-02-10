cd9fe35660ecf173e0d677b5224ff2ebe1f0afe6
Jack Neilson <jackdneilson@gmail.com>
2025-02-10 15:29:02 +0000

    feat: Add solutions for exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+	if (n < 0) return "OOPS";
+	if (n == 0) return 0;
+	if (n <= 2) return 1;
+	else return fibonacci(n - 2) + fibonacci(n - 1);
 };
 
 // Do not edit below this line
